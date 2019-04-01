79c39bc71aa3a4058a8fb51405e8349d769111b0
Lukas <lukasyoga.permana@mitrais.com>
2019-04-01 08:24:04 +0800

    add button on nav bar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthenticationService} from '../service/authentication.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +7,71 @@ import {AuthenticationService} from '../service/authentication.service';
 })
 export class NavComponent implements OnInit {
   appTitle = 'Mitrais Carrot';
+  jsonNav: any;
+  navEmp: any;
   constructor() { }
 
   ngOnInit() {
-    console.log('naaavvvv')
+    const pillElementMgr = document.querySelector('#linkManager');
+    const pillElementEmp = document.querySelector('#linkEmployee');
+    const pillElementMrc = document.querySelector('#linkMerchant');
+    const pillElementFrm = document.querySelector('#linkFarmer');
+    const pillElementAdm = document.querySelector('#linkAdmin');
+
+    this.jsonNav = JSON.parse(localStorage.currentUser);
+    this.navEmp = JSON.stringify(this.jsonNav.role);
+    console.log('this navemp:  ' + this.navEmp);
+
+    switch (this.navEmp) {
+      case(''):
+      pillElementEmp.remove();
+      case('"STAFF"'):
+      console.log('ag');
+      pillElementAdm.remove();
+      pillElementFrm.remove();
+      pillElementMgr.remove();
+      pillElementMrc.remove();
+      break;
+
+      case('"SENIOR_MANAGER"'):
+      case('"MANAGER"'):
+      pillElementAdm.remove();
+      pillElementFrm.remove();
+      pillElementMrc.remove();
+      break;
+
+      case('"ADMIN"'):
+      pillElementMgr.remove();
+      break;
+
+    }
+
+    switch (window.location.pathname) {
+      case ('/manager'):
+      pillElementMgr.className = 'nav-link active';
+      pillElementMgr.removeAttribute('href');
+      break;
+
+      case ('/employee'):
+      pillElementEmp.className = 'nav-link active';
+      pillElementEmp.removeAttribute('href');
+      break;
+
+      case ('/merchant'):
+      pillElementMrc.className = 'nav-link active';
+      pillElementMrc.removeAttribute('href');
+      break;
+
+      case ('/farmer'):
+      pillElementFrm.className = 'nav-link active';
+      pillElementFrm.removeAttribute('href');
+      break;
+
+      case ('/administrator'):
+      pillElementAdm.className = 'nav-link active';
+      pillElementAdm.removeAttribute('href');
+      break;
+    }
   }
 
   goLogout(){
