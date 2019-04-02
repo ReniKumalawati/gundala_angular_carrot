26c8eccf5bc256adcd2077c050ffe3f8964bbb7b
latudimas <riswandhalatu.dimas@mitrais.com>
2019-04-02 18:04:44 +0800

    fixing conflict issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,14 +34,11 @@ import { MerchantTransactionComponent } from './merchant/merchant-transaction/me
 import { GroupSocialFoundationComponent } from './manager/detail-group/group-social-foundation/group-social-foundation.component';
 import { AchievementComponent } from './employee/achievement/achievement.component';
 import { ClaimComponent } from './manager/claim/claim.component';
-<<<<<<< HEAD
 import { SeniorManagerComponent } from './manager/senior-manager/senior-manager.component';
-=======
 import { ModalLoadingComponent } from './partial/modal-loading/modal-loading.component';
 import { AchievementListComponent } from './employee/achievement/achievement-list/achievement-list.component';
 import { AchievedComponent } from './employee/achievement/achieved/achieved.component';
 import {AuthService} from './interceptors/auth.service';
->>>>>>> b2b6983033ecf5cc6051938b42fd50fec31cb1c3
 
 @NgModule({
   declarations: [
