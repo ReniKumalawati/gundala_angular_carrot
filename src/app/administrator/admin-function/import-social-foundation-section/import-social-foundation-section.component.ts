94e5b79884c78275eaadfcd0c2b2b886a5b86cef
latudimas <riswandha.ld@gmail.com>
2019-04-09 21:20:05 +0700

    social foundation image [still not working]

--- src/app/administrator/admin-function/import-social-foundation-section/import-social-foundation-section.component.ts
+++ src/app/administrator/admin-function/import-social-foundation-section/import-social-foundation-section.component.ts
@@ -3,43 +3,62 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { SocialFoundationService } from '../../../service/social-foundation.service';
 
-
 @Component({
-  selector: 'app-import-social-foundation-section',
-  templateUrl: './import-social-foundation-section.component.html',
-  styleUrls: ['./import-social-foundation-section.component.scss']
+  selector: "app-import-social-foundation-section",
+  templateUrl: "./import-social-foundation-section.component.html",
+  styleUrls: ["./import-social-foundation-section.component.scss"]
 })
 export class ImportSocialFoundationSectionComponent implements OnInit {
+  id:any;
+  socialFoundation:any;
   sfTemp: Object;
   submitted = false;
   socialFoundationData: any;
   messageForm: FormGroup;
-  formSocialFoundation = {name: '', description: '', min_carrot: '', total_carrot: 0, status: false, id: ''};
+  base64Image: string;
+  imageSrc: any;
+
+  formSocialFoundation = {
+    name: '',
+    description: '',
+    pictureUrl: '',
+    min_carrot: '',
+    total_carrot: 0,
+    status: false,
+    id: ''
+  };
+
   constructor(
-    private data: SocialFoundationService, 
-    private modalService: NgbModal, 
+    private socialFoundationService: SocialFoundationService,
+    private modalService: NgbModal,
     private formBuilder: FormBuilder
-    ) { }
+  ) {}
 
   ngOnInit() {
-    console.log('cccccc');
     this.messageForm = this.formBuilder.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
-      min_carrot: ['', Validators.required],
+      pictureUrl: ['', Validators.required],
+      min_carrot: ['', Validators.required]
     });
     this.findAllSocialFoundation();
   }
+
   findAllSocialFoundation() {
-    this.data.findAllSocialFoundation().subscribe(callback => {
+    this.socialFoundationService.findAllSocialFoundation().subscribe(callback => {
       this.socialFoundationData = callback;
-      this.socialFoundationData = this.socialFoundationData.listSocialFoundation
-      console.log('dddd');
+      this.socialFoundationData = this.socialFoundationData.listSocialFoundation;
+      // console.log("dddd");
     });
   }
+
   open(content) {
     this.modalService.open(content);
   }
+
+  // retrieveSocialFoundation() {
+  //   this.socialFou
+  // }
 
   close() {
     this.messageForm.reset();
@@ -53,25 +72,48 @@ export class ImportSocialFoundationSectionComponent implements OnInit {
       // alert('please fulfill the form first');
       return;
     }
-    console.log(this.formSocialFoundation.id)
-    if (this.formSocialFoundation.id != undefined && this.formSocialFoundation.id != '') {
-      let id: any;
-      id = this.formSocialFoundation.id;
+    console.log(this.formSocialFoundation.id);
+    if (this.formSocialFoundation.id != undefined && this.formSocialFoundation.id != "") {
+      // let id: any;
+      this.id = this.formSocialFoundation.id;
       delete this.formSocialFoundation.id;
-      this.data.updateSocialFoundation(this.formSocialFoundation, id).subscribe(callback => {
+      this.socialFoundationService.updateSocialFoundation(this.formSocialFoundation, this.id).subscribe(callback => {
         let kembalian: any;
         kembalian = callback;
-        this.findAllSocialFoundation();
-        this.close();
-      });
+        console.log(kembalian);
+        if (this.base64Image !== '') {
+            this.socialFoundationService.uploadImage(this.id, {img : this.base64Image}).subscribe(imageCallback => {
+              this.id = '';
+              this.findAllSocialFoundation();
+              this.close();
+            });
+          } else {
+            this.findAllSocialFoundation();
+            this.close();
+          }
+          // let kembalian: any;
+          // kembalian = callback;
+          // console.log(kembalian);
+          // this.findAllSocialFoundation();
+          // this.close();
+        });
     } else {
       delete this.formSocialFoundation.id;
-      this.data.insertSocialFoundationIntoDB(this.formSocialFoundation).subscribe(callback => {
-        let kembalian: any;
-        kembalian = callback;
-        this.findAllSocialFoundation();
-        this.close();
-      });
+      this.socialFoundationService.insertSocialFoundationIntoDB(this.formSocialFoundation).subscribe(callback => {
+          let kembalian: any;
+          kembalian = callback;
+          if(this.base64Image !== '') {
+            this.socialFoundationService.uploadImage(this.id, {img : this.base64Image}).subscribe(imageCallback => {
+              this.findAllSocialFoundation();
+              this.close();
+            })
+          } else {
+            this.findAllSocialFoundation();
+            this.close();
+          }
+          // this.findAllSocialFoundation();
+          // this.close();
+        });
     }
   }
 
@@ -83,27 +125,44 @@ export class ImportSocialFoundationSectionComponent implements OnInit {
     }
 
     console.log(socialFoundation);
-    this.data.updateSocialFoundation(socialFoundation, socialFoundation.id).subscribe(callback => {
+    this.socialFoundationService
+      .updateSocialFoundation(socialFoundation, socialFoundation.id)
+      .subscribe(callback => {
+        this.findAllSocialFoundation();
+        this.close();
+      });
+  }
+
+  openEditModal(data, content) {
+    console.log(data);
+    this.sfTemp = data.id;
+    this.formSocialFoundation.name = data.name;
+    this.formSocialFoundation.description = data.description;
+    this.formSocialFoundation.pictureUrl = data.pictureUrl;
+    this.formSocialFoundation.min_carrot = data.min_carrot;
+    this.formSocialFoundation.status = data.status;
+    this.formSocialFoundation.id = data.id;
+    this.open(content);
+    // console.log(this.formSocialFoundation);
+  }
+
+  removeSocialFoundationFromDB(id) {
+    this.socialFoundationService.deleteSocialFoundationFromDB(id).subscribe(callback => {
       this.findAllSocialFoundation();
       this.close();
     });
   }
 
-  openEditModal(data, content) {
-    this.sfTemp = data.id;
-    this.formSocialFoundation.name = data.name;
-    this.formSocialFoundation.description = data.description;
-    this.formSocialFoundation.min_carrot = data.min_carrot;
-    this.formSocialFoundation.status = data.status;
-    this.formSocialFoundation.id = data.id;
-    this.open(content);
-  }
-
-  removeSocialFoundationFromDB(id) {
-    this.data.deleteSocialFoundationFromDB(id).subscribe(callback => {
-      this.findAllSocialFoundation();
-      this.close();
-    })
+  onFileChange(event) {
+    let reader = new FileReader();
+    if(event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageSrc = reader.result.toString();
+        this.base64Image = reader.result.toString().split(',')[1];
+      };
+    }
   }
 
 }
