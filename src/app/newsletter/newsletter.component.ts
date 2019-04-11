057fa318ff89f09377e049e0e3adc7c5c6b79772
latudimas <riswandhalatu.dimas@mitrais.com>
2019-04-11 16:39:20 +0800

    newsletter page

--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, Form} from '@angular/forms';
+import { from } from 'rxjs';
+import { NewsletterService } from '../service/newsletter.service';
 
 @Component({
   selector: 'app-newsletter',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newsletter.component.scss']
 })
 export class NewsletterComponent implements OnInit {
+  currentUser: any;
+  name: string;
 
-  constructor() { }
+  form: FormGroup;
+
+  newsletterForm: {
+    newsletterSubject: '',
+    newsletterContent: '',
+    senderName: ''
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private newsletterService: NewsletterService
+  ) { }
 
   ngOnInit() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(this.currentUser.name);
+    this.name = this.currentUser.name;
+
+    this.form = this.formBuilder.group({
+      subject: ['', Validators.required],
+      content: ['', Validators.required],
+    });
   }
 
+  sendNewsletter() {
+    // this.newsletterService.postNewsletter(data).subscribe()
+  }
 }
